47afa7664aeab40cc724eb14aefdc985da2b4120
iSplasher <isplasher@gmail.com>
2019-12-12 14:53:46 +0100

    commissiong negotiate;

---
+++ components/Form/PriceSuggestionForm.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Form, Schema, FormGroup, ControlLabel, FormControl, InputNumber, Button, Message } from 'rsuite';
+
+import { t } from '@app/utility/lang'
+import { get_profile_name, decimal128ToMoneyToString, price_is_null, decimal128ToFloat } from '@utility/misc';
+
+const {
+    StringType,
+    NumberType,
+    BooleanType,
+    ArrayType,
+    ObjectType,
+} = Schema.Types
+
+const model = Schema.Model({
+    new_price: NumberType().isRequired('This field is required.')
+        .addRule((value, data) => {
+            if (value < 0) return false
+            return true
+        }, t`No negatives allowed`),
+})
+
+interface Props {
+    user: any
+    price: any
+    waiting?: boolean
+    onAcceptPrice?: Function
+    onSuggestPrice?: (price: number) => void
+}
+
+const PriceSuggestionForm = (props: Props) => {
+
+    const name = get_profile_name(props.user)
+    const [form_value, set_form_value] = useState({} as any)
+    const [form_ref, set_form_ref] = useState(null)
+    const [accept_loading, set_accept_loading] = useState(false)
+    const [suggest_loading, set_suggest_loading] = useState(false)
+
+    return (
+        <Form model={model} formValue={form_value} onChange={value => set_form_value(value)} ref={ref => set_form_ref(ref)}>
+            {price_is_null(props.price) && <h4>{t`A price for this request needs to be set`}</h4>}
+            {!price_is_null(props.price) && <h4>{t`${name} has suggested a price`}</h4>}
+            {!price_is_null(props.price) &&
+            <FormGroup>
+                <h5>{t`Suggested price`}: <span className="text-primary">{decimal128ToMoneyToString(props.price)}</span></h5>
+            </FormGroup>
+            }
+            {props.waiting && <Message type="info" description={t`Waiting for price confirmation`}/>}
+            {!props.waiting &&
+            <>
+                <FormGroup>
+                    <ControlLabel>{t`Suggest a new price`}</ControlLabel>
+                    <FormControl
+                        name="new_price"
+                        prefix="$"
+                        accepter={InputNumber}
+                        type="number"
+                    />
+                </FormGroup>
+                {!price_is_null(props.price) && <Button loading={accept_loading} onClick={(ev) => {ev.preventDefault(); set_accept_loading(true); if (props.onAcceptPrice) props.onAcceptPrice()}} appearance="primary" type="button" className="mr-2">{t`Accept suggested price`}</Button>}
+                <Button loading={suggest_loading} type="submit" onClick={(ev) => {
+                    ev.preventDefault()
+                    if (form_ref && form_ref.check()) {
+                        if (!price_is_null(props.price) && form_value.new_price === decimal128ToFloat(props.price)) {
+                            return
+                        }
+                        if (props.onSuggestPrice) {
+                            set_suggest_loading(true)
+                            props.onSuggestPrice(form_value.new_price)
+                        }
+                    }
+                    }}>{t`Suggest price`}</Button>
+            </>}
+        </Form>
+    );
+};
+
+export default PriceSuggestionForm;
---
+++ pages/api/commission/suggest_price.ts
@@ -0,0 +1,33 @@
+import { BAD_REQUEST, OK, CREATED, NOT_FOUND } from 'http-status-codes'
+import microCors from 'micro-cors'
+import mongoose, { Document } from 'mongoose'
+
+import { error_message, data_message } from '@utility/message'
+import {
+    with_auth_middleware,
+    ExApiRequest,
+    ExApiResponse,
+} from '@server/middleware'
+import { accept_commission_price, suggest_commission_price } from '@services/commission'
+
+const cors = microCors({ allowMethods: ['PUT', 'POST', 'OPTIONS'] })
+
+export default with_auth_middleware(
+    async (req: ExApiRequest, res: ExApiResponse) => {
+        try {
+            const { accept, new_price, commission_id } = req.json
+
+            let r
+            if (accept) {
+                r = await accept_commission_price(commission_id)
+            } else {
+                r = await suggest_commission_price(req.user, commission_id, new_price)
+            }
+
+            res.status(OK).json(data_message(r))
+
+        } catch (err) {
+            res.status(BAD_REQUEST).json(error_message(err.message))
+        }
+    }
+)
